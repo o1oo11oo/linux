79ba34e7658cb8edc8cd24bb5d46bce9d55a3fec
Lukas Fischer <kernel@o1oo11oo.de>
2025-03-08 18:13:18 +0100

    dabac_rs: Explicitly use bindings
    Whenever a type from kernel::bindings is used, keep the bindings::
    prefix so that it's clear it's a reference to something from there.
    
    Signed-off-by: Lukas Fischer <kernel@o1oo11oo.de>

--- security/dabac_rs/bindings.rs
+++ security/dabac_rs/bindings.rs
@@ -7,11 +7,9 @@
 //! for now this is easier here.
 
 use kernel::{
-    bindings::{
-        lsm_id, lsm_info, lsm_order_LSM_ORDER_MUTABLE, security_add_hooks, security_hook_list,
-        security_list_options, static_calls_table, LSM_ID_DABAC_RS,
-    },
-    c_str,
+    bindings, c_str,
+    ffi::*,
+    fs::File,
     prelude::*,
     types::Opaque,
 };
@@ -27,7 +25,7 @@ const SECURITY_HOOK_LIST_LEN: usize = 1;
 
 /// Wrapper to be able to use `lsm_id` in a static context.
 #[repr(transparent)]
-struct LsmId(Opaque<lsm_id>);
+struct LsmId(Opaque<bindings::lsm_id>);
 unsafe impl Sync for LsmId {}
 
 /// Wrapper to be able to use `lsm_info` in a static context.
@@ -35,28 +33,28 @@ unsafe impl Sync for LsmId {}
 // cannot express this and even statically setting it to 8 cannot be combined
 // with repr(transparent).
 #[repr(transparent)]
-struct LsmInfo(Opaque<lsm_info>);
+struct LsmInfo(Opaque<bindings::lsm_info>);
 unsafe impl Sync for LsmInfo {}
 
 /// Wrapper to be able to use `security_hook_list` in a static context.
 #[repr(transparent)]
-struct SecurityHookList(Opaque<[security_hook_list; SECURITY_HOOK_LIST_LEN]>);
+struct SecurityHookList(Opaque<[bindings::security_hook_list; SECURITY_HOOK_LIST_LEN]>);
 unsafe impl Sync for SecurityHookList {}
 
 /// Static information about the LSM.
-static DABAC_RS_LSMID: LsmId = LsmId(Opaque::new(lsm_id {
+static DABAC_RS_LSMID: LsmId = LsmId(Opaque::new(bindings::lsm_id {
     name: NAME.as_char_ptr(),
-    id: LSM_ID_DABAC_RS as _,
+    id: bindings::LSM_ID_DABAC_RS as _,
 }));
 
 /// Registers the LSM in the kernel by placing it in the `.lsm_info.init` linker
 /// section.
 #[used]
 #[link_section = ".lsm_info.init"]
-static DABAC_RS_LSMINFO: LsmInfo = LsmInfo(Opaque::new(lsm_info {
+static DABAC_RS_LSMINFO: LsmInfo = LsmInfo(Opaque::new(bindings::lsm_info {
     name: NAME.as_char_ptr(),
     init: Some(dabac_rs_init),
-    order: lsm_order_LSM_ORDER_MUTABLE,
+    order: bindings::lsm_order_LSM_ORDER_MUTABLE,
     flags: 0,
     enabled: core::ptr::null_mut(),
     blobs: core::ptr::null_mut(),
@@ -66,12 +64,12 @@ static DABAC_RS_LSMINFO: LsmInfo = LsmInfo(Opaque::new(lsm_info {
 /// `lsm_info`.
 #[no_mangle]
 #[link_section = ".init.text"]
-pub extern "C" fn dabac_rs_init() -> kernel::ffi::c_int {
+pub extern "C" fn dabac_rs_init() -> c_int {
     pr_info!("Rust DABAC LSM is starting...\n");
 
     // Register hooks
     unsafe {
-        security_add_hooks(
+        bindings::security_add_hooks(
             &raw mut DABAC_RS_HOOKS.0 as _,
             SECURITY_HOOK_LIST_LEN as _,
             DABAC_RS_LSMID.0.get(),
@@ -92,22 +90,20 @@ pub extern "C" fn dabac_rs_init() -> kernel::ffi::c_int {
 /// `SECURITY_HOOK_LIST_LEN`.
 #[used]
 #[link_section = ".data..ro_after_init"]
-static mut DABAC_RS_HOOKS: SecurityHookList = SecurityHookList(Opaque::new([security_hook_list {
-    scalls: unsafe { &raw mut static_calls_table.file_permission as _ },
-    hook: security_list_options {
-        file_permission: Some(dabac_rs_file_permission),
-    },
-    lsmid: DABAC_RS_LSMID.0.get(),
-}]));
+static mut DABAC_RS_HOOKS: SecurityHookList =
+    SecurityHookList(Opaque::new([bindings::security_hook_list {
+        scalls: unsafe { &raw mut bindings::static_calls_table.file_permission as _ },
+        hook: bindings::security_list_options {
+            file_permission: Some(dabac_rs_file_permission),
+        },
+        lsmid: DABAC_RS_LSMID.0.get(),
+    }]));
 
 /// Callback for the `file_permission` hook, gets called every time a file is
 /// read or written.
 #[no_mangle]
-pub extern "C" fn dabac_rs_file_permission(
-    file: *mut kernel::bindings::file,
-    mask: kernel::ffi::c_int,
-) -> kernel::ffi::c_int {
-    let file = unsafe { kernel::fs::File::from_raw_file(file) };
+pub extern "C" fn dabac_rs_file_permission(file: *mut bindings::file, mask: c_int) -> c_int {
+    let file = unsafe { File::from_raw_file(file) };
 
     match pdp::file_permission(file, mask) {
         Ok(allowed) => {
