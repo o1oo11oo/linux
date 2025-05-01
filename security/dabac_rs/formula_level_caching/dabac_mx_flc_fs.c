48aed80a2f8a79670ef17228670e48da62780520
Lukas Fischer <kernel@o1oo11oo.de>
2025-05-01 23:40:47 +0200

    dabac_rs: Use individual paths in securityfs
    To fix the last blocker for the LSMs actually working together, choose a
    unique path where each of the variants places its files in the
    securityfs, for example dabac_rs_mx_nc. If only one variant is loaded,
    the generic dabac_rs directory is used in addition to simplify the
    evaluation, since the code then does not have to distinguish between the
    variants for that.
    
    Signed-off-by: Lukas Fischer <kernel@o1oo11oo.de>

--- security/dabac_rs/formula_level_caching/dabac_mx_flc_fs.c
+++ security/dabac_rs/formula_level_caching/dabac_mx_flc_fs.c
@@ -13,6 +13,12 @@ struct dentry *dabac_rs_mx_flc_obj_attr_file;
 struct dentry *dabac_rs_mx_flc_env_attr_file;
 struct dentry *dabac_rs_mx_flc_policy_file;
 
+struct dentry *dabac_rs_mx_flc_generic_fs;
+struct dentry *dabac_rs_mx_flc_generic_user_attr_file;
+struct dentry *dabac_rs_mx_flc_generic_obj_attr_file;
+struct dentry *dabac_rs_mx_flc_generic_env_attr_file;
+struct dentry *dabac_rs_mx_flc_generic_policy_file;
+
 // The unsafety of these functions is somewhat hidden by the linker, in C the
 // function definitions contain a pointer argument, while the same argument is
 // represented as UserPtr (usize) in Rust. Since there is no unsafe function to
@@ -27,7 +33,16 @@ extern ssize_t dabac_rs_mx_flc_update_env_attr(struct file *filp, const char __u
 extern ssize_t dabac_rs_mx_flc_read_policy(struct file *filp, char __user *buffer, size_t count, loff_t *off);
 extern ssize_t dabac_rs_mx_flc_update_policy(struct file *filp, const char __user *buffer, size_t len, loff_t *off);
 
+extern int dabac_rs_mx_nc_initialized;
+extern int dabac_rs_sl_nc_initialized;
+extern int dabac_rs_mx_tlc_initialized;
+extern int dabac_rs_sl_tlc_initialized;
+extern int dabac_rs_mx_tlch_initialized;
+extern int dabac_rs_sl_tlch_initialized;
 int dabac_rs_mx_flc_initialized;
+extern int dabac_rs_sl_flc_initialized;
+extern int dabac_rs_mx_flch_initialized;
+extern int dabac_rs_sl_flch_initialized;
 
 void dabac_rs_mx_flc_init_done(void);
 void dabac_rs_mx_flc_init_done(void)
@@ -77,60 +92,131 @@ static void dabac_rs_mx_flc_destroy_fs(void)
 		securityfs_remove(dabac_rs_mx_flc_policy_file);
 	if (!IS_ERR_OR_NULL(dabac_rs_mx_flc_fs))
 		securityfs_remove(dabac_rs_mx_flc_fs);
+	if (!IS_ERR_OR_NULL(dabac_rs_mx_flc_generic_user_attr_file))
+		securityfs_remove(dabac_rs_mx_flc_generic_user_attr_file);
+	if (!IS_ERR_OR_NULL(dabac_rs_mx_flc_generic_obj_attr_file))
+		securityfs_remove(dabac_rs_mx_flc_generic_obj_attr_file);
+	if (!IS_ERR_OR_NULL(dabac_rs_mx_flc_generic_env_attr_file))
+		securityfs_remove(dabac_rs_mx_flc_generic_env_attr_file);
+	if (!IS_ERR_OR_NULL(dabac_rs_mx_flc_generic_policy_file))
+		securityfs_remove(dabac_rs_mx_flc_generic_policy_file);
+	if (!IS_ERR_OR_NULL(dabac_rs_mx_flc_generic_fs))
+		securityfs_remove(dabac_rs_mx_flc_generic_fs);
 }
 
-static struct dentry *dabac_rs_mx_flc_create_file(const char *filename, const struct file_operations *fops)
+static struct dentry *dabac_rs_mx_flc_create_file(struct dentry *parent, const char *parentname, const char *filename, const struct file_operations *fops)
 {
 	struct dentry *f;
 
 	// File is writable by everyone because the PAP checks each access with the PDP
-	f = securityfs_create_file(filename, 0666, dabac_rs_mx_flc_fs, NULL, fops);
+	f = securityfs_create_file(filename, 0666, parent, NULL, fops);
 	if (IS_ERR(f)) {
-		pr_err("dabac_rs_mx_flc: Failed to create file /sys/kernel/security/dabac_rs/%s", filename);
+		pr_err("dabac_rs_mx_flc: Failed to create file /sys/kernel/security/%s/%s", parentname, filename);
 		dabac_rs_mx_flc_destroy_fs();
 		return f;
 	}
-	pr_info("dabac_rs_mx_flc: Created file /sys/kernel/security/dabac_rs/%s", filename);
+	pr_info("dabac_rs_mx_flc: Created file /sys/kernel/security/%s/%s", parentname, filename);
 	return f;
 }
 
 static int dabac_rs_mx_flc_create_fs(void)
 {
+	const char *parentname = "dabac_rs_mx_flc";
+
 	if (!dabac_rs_mx_flc_initialized) {
 		pr_info("dabac_rs_mx_flc: LSM was not initialized, not loading securityfs");
 		return 0;
 	}
 
 	// create the root 'dabac_rs' directory
-	dabac_rs_mx_flc_fs = securityfs_create_dir("dabac_rs", NULL);
+	dabac_rs_mx_flc_fs = securityfs_create_dir(parentname, NULL);
 	if (IS_ERR(dabac_rs_mx_flc_fs)) {
-		pr_err("dabac_rs_mx_flc: Failed to create dabac_rs securityfs at /sys/kernel/security/dabac_rs/");
+		pr_err("dabac_rs_mx_flc: Failed to create dabac_rs securityfs at /sys/kernel/security/%s/", parentname);
 		dabac_rs_mx_flc_destroy_fs();
 		return PTR_ERR(dabac_rs_mx_flc_fs);
 	}
 
-	dabac_rs_mx_flc_user_attr_file = dabac_rs_mx_flc_create_file("user_attr", &user_attr_fops);
+	dabac_rs_mx_flc_user_attr_file = dabac_rs_mx_flc_create_file(dabac_rs_mx_flc_fs, parentname, "user_attr", &user_attr_fops);
 	if (IS_ERR(dabac_rs_mx_flc_user_attr_file)) {
 		dabac_rs_mx_flc_destroy_fs();
 		return PTR_ERR(dabac_rs_mx_flc_user_attr_file);
 	}
-	dabac_rs_mx_flc_obj_attr_file = dabac_rs_mx_flc_create_file("obj_attr", &obj_attr_fops);
+	dabac_rs_mx_flc_obj_attr_file = dabac_rs_mx_flc_create_file(dabac_rs_mx_flc_fs, parentname, "obj_attr", &obj_attr_fops);
 	if (IS_ERR(dabac_rs_mx_flc_obj_attr_file)) {
 		dabac_rs_mx_flc_destroy_fs();
 		return PTR_ERR(dabac_rs_mx_flc_obj_attr_file);
 	}
-	dabac_rs_mx_flc_env_attr_file = dabac_rs_mx_flc_create_file("env_attr", &env_attr_fops);
+	dabac_rs_mx_flc_env_attr_file = dabac_rs_mx_flc_create_file(dabac_rs_mx_flc_fs, parentname, "env_attr", &env_attr_fops);
 	if (IS_ERR(dabac_rs_mx_flc_env_attr_file)) {
 		dabac_rs_mx_flc_destroy_fs();
 		return PTR_ERR(dabac_rs_mx_flc_env_attr_file);
 	}
-	dabac_rs_mx_flc_policy_file = dabac_rs_mx_flc_create_file("policy", &policy_fops);
+	dabac_rs_mx_flc_policy_file = dabac_rs_mx_flc_create_file(dabac_rs_mx_flc_fs, parentname, "policy", &policy_fops);
 	if (IS_ERR(dabac_rs_mx_flc_policy_file)) {
 		dabac_rs_mx_flc_destroy_fs();
 		return PTR_ERR(dabac_rs_mx_flc_policy_file);
 	}
 
+	pr_info("dabac_rs_mx_flc: securityfs initialized");
+	return 0;
+}
+
+static int dabac_rs_mx_flc_create_generic_fs(void)
+{
+	const char *parentname = "dabac_rs";
+
+	if (!dabac_rs_mx_flc_initialized) {
+		pr_info("dabac_rs_mx_flc: LSM was not initialized, not loading securityfs");
+		return 0;
+	}
+
+	if (dabac_rs_mx_nc_initialized
+	    || dabac_rs_sl_nc_initialized
+	    || dabac_rs_mx_tlc_initialized
+	    || dabac_rs_sl_tlc_initialized
+	    || dabac_rs_mx_tlch_initialized
+	    || dabac_rs_sl_tlch_initialized
+	    || dabac_rs_sl_flc_initialized
+	    || dabac_rs_mx_flch_initialized
+	    || dabac_rs_sl_flch_initialized) {
+		pr_info("dabac_rs_mx_flc: We are not the only dabac_rs LSM, not using generic paths");
+		return 0;
+	}
+
+	pr_info("dabac_rs_mx_flc: We are the only dabac_rs LSM, using generic paths additionally");
+
+	// create the root 'dabac_rs' directory
+	dabac_rs_mx_flc_generic_fs = securityfs_create_dir(parentname, NULL);
+	if (IS_ERR(dabac_rs_mx_flc_generic_fs)) {
+		pr_err("dabac_rs_mx_flc: Failed to create dabac_rs securityfs at /sys/kernel/security/%s/", parentname);
+		dabac_rs_mx_flc_destroy_fs();
+		return PTR_ERR(dabac_rs_mx_flc_generic_fs);
+	}
+
+	dabac_rs_mx_flc_generic_user_attr_file = dabac_rs_mx_flc_create_file(dabac_rs_mx_flc_generic_fs, parentname, "user_attr", &user_attr_fops);
+	if (IS_ERR(dabac_rs_mx_flc_generic_user_attr_file)) {
+		dabac_rs_mx_flc_destroy_fs();
+		return PTR_ERR(dabac_rs_mx_flc_generic_user_attr_file);
+	}
+	dabac_rs_mx_flc_generic_obj_attr_file = dabac_rs_mx_flc_create_file(dabac_rs_mx_flc_generic_fs, parentname, "obj_attr", &obj_attr_fops);
+	if (IS_ERR(dabac_rs_mx_flc_generic_obj_attr_file)) {
+		dabac_rs_mx_flc_destroy_fs();
+		return PTR_ERR(dabac_rs_mx_flc_generic_obj_attr_file);
+	}
+	dabac_rs_mx_flc_generic_env_attr_file = dabac_rs_mx_flc_create_file(dabac_rs_mx_flc_generic_fs, parentname, "env_attr", &env_attr_fops);
+	if (IS_ERR(dabac_rs_mx_flc_generic_env_attr_file)) {
+		dabac_rs_mx_flc_destroy_fs();
+		return PTR_ERR(dabac_rs_mx_flc_generic_env_attr_file);
+	}
+	dabac_rs_mx_flc_generic_policy_file = dabac_rs_mx_flc_create_file(dabac_rs_mx_flc_generic_fs, parentname, "policy", &policy_fops);
+	if (IS_ERR(dabac_rs_mx_flc_generic_policy_file)) {
+		dabac_rs_mx_flc_destroy_fs();
+		return PTR_ERR(dabac_rs_mx_flc_generic_policy_file);
+	}
+
+	pr_info("dabac_rs_mx_flc: generic securityfs initialized");
 	return 0;
 }
 
 fs_initcall(dabac_rs_mx_flc_create_fs);
+fs_initcall(dabac_rs_mx_flc_create_generic_fs);
